4552858235461bf9a89903faa938f74f32a9fd9f
Seiki Tokunaga <seiki.tokunaga@riken.jp>
2023-08-05 15:36:34 +0900

    webhookの確認応答処理の実装

--- index.js
+++ index.js
@@ -11,15 +11,12 @@ const config = {
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
+app.get('/', (req, res) => res.send('Hello  BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
 app.post('/webhook', line.middleware(config), (req, res) => {
-	console.log(req.body.events);
-
-	//ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
-	if (req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff') {
-		res.send('Hello LINE BOT!(POST)');
-		console.log('疎通確認用');
-		return;
+	console.log(`webhookメッセージを受信 ${req.body.events}`);
+	if (req.body.events == []){
+		console.log('webhookの確認通知メッセージを受信')
+		return Promise.resolve(null);
 	}
 
 	Promise
